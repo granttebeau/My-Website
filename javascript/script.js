83cdcc761db78d2a428a40375c0b56c5783455cc
Grant Tebeau <grant_tebeau@yahoo.com>
2020-04-05 22:09:26 -0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,7 @@ var prevPosition = window.pageYOffset;
 // updates the position fo teh topRect div
 window.onscroll = function() {
 var curPosition = window.pageYOffset;
-  if (prevPosition > curPosition || prevPosition == 0) {
+  if (prevPosition > curPosition || prevPosition <= 0) {
     document.getElementById("topRect").style.top = "0";
   } else {
     document.getElementById("topRect").style.top = "-75px";
