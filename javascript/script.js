8841d32f593842de163bcb5a38f7a1ceeadbc4ef
Grant Tebeau <grant_tebeau@yahoo.com>
2020-04-05 22:05:38 -0500

    fixes scrolling

--- javascript/script.js
+++ javascript/script.js
@@ -2,10 +2,10 @@
 // gets the previous page position
 var prevPosition = window.pageYOffset;
 
-// updates the position fo teh topRect div 
+// updates the position fo teh topRect div
 window.onscroll = function() {
 var curPosition = window.pageYOffset;
-  if (prevPosition > curPosition) {
+  if (prevPosition > curPosition || prevPosition == 0) {
     document.getElementById("topRect").style.top = "0";
   } else {
     document.getElementById("topRect").style.top = "-75px";
